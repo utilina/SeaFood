e78bb280d8c96348c256feabc236d290368fa2c3
Anastasiya Ulitina <anastulitina@gmail.com>
2020-11-13 21:11:26 +0300

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Анастасия Улитина on 13.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
